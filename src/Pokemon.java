5f1b0ea31a17a11b5a422e77d1dbe9dcb2ff37b1
Rachid <rachidzahar@hotmail.fr>
2021-10-14 10:57:16 +0200

    class pokemon + commentaires

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,9 +1,11 @@
 
 public class Pokemon {
+	//Définition des caractéristiques
 	private String nom;
 	private int hp;
 	private int atk;
 
+	// Définition de la méthode Pokemon
 	public Pokemon() {
 	};
 
@@ -14,11 +16,25 @@ public class Pokemon {
 		this.atk = atk;
 	}
 
+	// On écrit la méthode "isDead" qui renverra true si les pv tombent à 0
 	public boolean isDead() {
 		if (hp == 0) {
+			System.out.println("Votre pokemon est KO");
 			return true;
-		} else {
-			return false;
+		}
+		return false;
 	}
-}
+	
+	// On créé la méthode "attaquer" qui servira à calculer la différence entre l'attaque de l'attaquant et les pv du défenseur
+	public double attaquer(Pokemon pokemon) {
+		double result = pokemon.hp - this.hp;
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Pokemon [nom=" + nom + ", hp=" + hp + ", atk=" + atk + "]";
+	}
+	
+	
 }
